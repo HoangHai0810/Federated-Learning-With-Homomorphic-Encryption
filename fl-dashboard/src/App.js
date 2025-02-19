536d4e28998aa4b50dabf3ed2836419bee1bb277
HoangHai0810 <106301328+HoangHai0810@users.noreply.github.com>
2025-02-19 14:39:57 +0700

    Done

---
+++ fl-dashboard/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TrainingDashboard from "./TrainingDashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <TrainingDashboard />
+    </div>
+  );
+}
+
+export default App;
